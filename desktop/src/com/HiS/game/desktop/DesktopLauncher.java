2bc6d3c76a389521c6859ac8eeea3be79ddb1aac
Morten Oftedal <mortenoftedal100@gmail.com>
2014-09-08 15:49:19 +0200

    Added libDGX project files

---
+++ desktop/src/com/HiS/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.HiS.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.HiS.game.MyHorseGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyHorseGame(), config);
+	}
+}
---
+++ html/src/com/HiS/game/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.HiS.game.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.HiS.game.MyHorseGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new MyHorseGame();
+        }
+}
