2a4f16fef9fae7353c60fbcac724e2317c8b183f
Vemund <vaa005@student.uib.no>
2014-09-16 13:24:32 +0200

    obstacle and fence

---
+++ core/src/com/HiS/gameobject/Fence.java
@@ -0,0 +1,11 @@
+package com.HiS.gameobject;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public class Fence extends Obstacle {
+
+	public Fence() {
+		super(null, 3, 7, 1000, 120, 10);
+		// TODO Auto-generated constructor stub
+	}
+}
---
+++ core/src/com/HiS/gameobject/Obstacle.java
@@ -0,0 +1,13 @@
+package com.HiS.gameobject;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public abstract class Obstacle extends PhysGameObject {
+
+	public Obstacle(TextureRegion texture, int width, int height, float weight,
+			float posX, float posY) {
+		super(texture, width, height, weight, posX, posY);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
