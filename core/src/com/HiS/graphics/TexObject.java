7a000fc4d27277064bb41b2388b4c10aa4754ea6
Morten Oftedal <mortenoftedal100@gmail.com>
2014-09-17 12:45:27 +0200

    Corrected mistake: staged texObject file.

---
+++ core/src/com/HiS/graphics/TexObject.java
@@ -0,0 +1,32 @@
+package com.HiS.graphics;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+public class TexObject implements GfxObject{
+
+	private TextureRegion texture;
+	
+	@Override
+	public TextureRegion getTexture() {
+		return this.texture;
+	}
+
+	@Override
+	public void setTexture(TextureRegion texture) {
+		this.texture = texture;
+	}
+
+	@Override
+	public Vector2 getPosition() {
+		return new Vector2(texture.getRegionX(), texture.getRegionY());
+	}
+
+	@Override
+	public void setPosition(int x, int y) {
+		this.texture.setRegionX(x);
+		this.texture.setRegionY(y);
+		
+	}
+
+}
