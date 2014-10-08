3b4617116f4fa49c64ed70e4b398a89c5197254c
Petter Rossebø <rossi89@gmail.com>
2014-10-08 12:43:00 +0200

    collision with floatyplatform

--- core/src/com/HiS/physics/Physics.java
+++ core/src/com/HiS/physics/Physics.java
@@ -5,19 +5,19 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 
 /**
- * Physics is an object which stores a game object's physical values.
- * It assumes all objects as rectangles
+ * Physics is an object which stores a game object's physical values. It assumes
+ * all objects as rectangles
  * 
  * @author lars, morten
  * @version 0.2
- *
+ * 
  */
 public class Physics {
 
 	private float weight;
 
 	private boolean isGrounded = true;
-	private boolean isOnTopOfObject = false;
+	private Physics onTopOfObject = null;
 
 	private Rectangle rect;
 	private Polygon poly;
@@ -28,18 +28,25 @@ public class Physics {
 
 	/**
 	 * Constructor for class Physics
-	 * @param width width of the object
-	 * @param height height of the object
-	 * @param weight weight of the object, used to offset jump height
-	 * @param posX position on screen, X axis
-	 * @param posY position on screen, Y axis
+	 * 
+	 * @param width
+	 *            width of the object
+	 * @param height
+	 *            height of the object
+	 * @param weight
+	 *            weight of the object, used to offset jump height
+	 * @param posX
+	 *            position on screen, X axis
+	 * @param posY
+	 *            position on screen, Y axis
 	 */
-	public Physics(int width, int height, float weight, float posX, float posY, Polygon poly) {
+	public Physics(int width, int height, float weight, float posX, float posY,
+			Polygon poly) {
 		this.weight = weight;
 
-		//Rectangle is used as an easier way to detect collisions in physEngine
-		this.rect = new Rectangle(posX,posY + height,width,height); 
-		
+		// Rectangle is used as an easier way to detect collisions in physEngine
+		this.rect = new Rectangle(posX, posY + height, width, height);
+
 		this.poly = poly;
 
 		this.position = new Vector2(posX, posY);
@@ -49,71 +56,105 @@ public class Physics {
 	}
 
 	/**
-	 * getter for the weight of the object
-	 * @return the current weight of the object
+	 * getter for the current acceleration of the object
+	 * 
+	 * @return the acceleration of the object
 	 */
-	public float getWeight() {
-		return weight;
+	public Vector2 getAcceleration() {
+		return this.acceleration;
+	}
+
+	public Physics getOnTopOfObject() {
+		return this.onTopOfObject;
+	}
+
+	public Polygon getPoly() {
+		return this.poly;
 	}
 
 	/**
-	 * setter for the weight parameter of the object
-	 * @param height the new weight for the object
+	 * getter for the objects current position
+	 * 
+	 * @return the position of the object
 	 */
-	public void setWeight(float weight) {
-		this.weight = weight;
-	}
-
-	/**
-	 * boolean to check if the object is on the ground
-	 * @return true if horse is on the ground, false if not
-	 */
-	public boolean isGrounded() {
-		return isGrounded;
-	}
-
-	/**
-	 * sets the value of grounded to true or false
-	 * @param isGrounded the new value for isGrounded
-	 */
-	public void setGrounded(boolean isGrounded) {
-		this.isGrounded = isGrounded;
-	}
-	
-	/**
-	 * boolean to check if the object is on top of another object
-	 * @return isOnTopOfObject the new value for isOnTopOfObject
-	 */
-	public boolean isOnTopOfObject() {
-		return isOnTopOfObject;
-	}
-	
-	/**
-	 * set the value of isOnTopOfObject to true or false
-	 * @param isOnTopOfObject the new value for isOnTopOfObject
-	 */
-	public void setOnTopOfObject(boolean isOnTopOfObject){
-		this.isOnTopOfObject = isOnTopOfObject;
+	public Vector2 getPosition() {
+		return this.position;
 	}
 
 	/**
 	 * Getter for the rectangle of the object
+	 * 
 	 * @return the rectangle
 	 */
 	public Rectangle getRect() {
-		return rect;
+		return this.rect;
 	}
 
 	/**
-	 * setter for the rectangle of the object
-	 * @param rect the new rectangle of the object
+	 * getter for the objects velocity
+	 * 
+	 * @return the objects velocity
 	 */
-	public void setRect(Rectangle rect) {
-		this.rect = rect;
+	public Vector2 getVelocity() {
+		return this.velocity;
 	}
 
-	public Polygon getPoly() {
-		return poly;
+	/**
+	 * getter for the weight of the object
+	 * 
+	 * @return the current weight of the object
+	 */
+	public float getWeight() {
+		return this.weight;
+	}
+
+	/**
+	 * boolean to check if the object is on the ground
+	 * 
+	 * @return true if horse is on the ground, false if not
+	 */
+	public boolean isGrounded() {
+		return this.isGrounded;
+	}
+
+	/**
+	 * boolean to check if the object is on top of another object
+	 * 
+	 * @return isOnTopOfObject the new value for isOnTopOfObject
+	 */
+	public boolean onTopOfObject() {
+
+		return this.onTopOfObject != null;
+	}
+
+	/**
+	 * setter for the objects acceleration
+	 * 
+	 * @param acceleration
+	 *            the new acceleration of the object
+	 */
+	public void setAcceleration(Vector2 acceleration) {
+		this.acceleration = acceleration;
+	}
+
+	/**
+	 * sets the value of grounded to true or false
+	 * 
+	 * @param isGrounded
+	 *            the new value for isGrounded
+	 */
+	public void setGrounded(boolean isGrounded) {
+		this.isGrounded = isGrounded;
+	}
+
+	/**
+	 * set the value of isOnTopOfObject to true or false
+	 * 
+	 * @param isOnTopOfObject
+	 *            the new value for isOnTopOfObject
+	 */
+	public void setOnTopOfObject(Physics onTopOfObject) {
+		this.onTopOfObject = onTopOfObject;
 	}
 
 	public void setPoly(Polygon poly) {
@@ -121,32 +162,10 @@ public class Physics {
 	}
 
 	/**
-	 * getter for the objects velocity
-	 * @return the objects velocity
-	 */
-	public Vector2 getVelocity() {
-		return velocity;
-	}
-
-	/**
-	 * setter for the objects velocity
-	 * @param velocity the new velocity of the object
-	 */
-	public void setVelocity(Vector2 velocity) {
-		this.velocity = velocity;
-	}
-
-	/**
-	 * getter for the objects current position
-	 * @return the position of the object
-	 */
-	public Vector2 getPosition() {
-		return position;
-	}
-
-	/**
 	 * setter for the objects position
-	 * @param position the new position
+	 * 
+	 * @param position
+	 *            the new position
 	 */
 	public void setPosition(Vector2 position) {
 		this.position = position;
@@ -155,18 +174,32 @@ public class Physics {
 	}
 
 	/**
-	 * getter for the current acceleration of the object
-	 * @return the acceleration of the object
+	 * setter for the rectangle of the object
+	 * 
+	 * @param rect
+	 *            the new rectangle of the object
 	 */
-	public Vector2 getAcceleration() {
-		return acceleration;
+	public void setRect(Rectangle rect) {
+		this.rect = rect;
 	}
 
 	/**
-	 * setter for the objects acceleration
-	 * @param acceleration the new acceleration of the object
+	 * setter for the objects velocity
+	 * 
+	 * @param velocity
+	 *            the new velocity of the object
 	 */
-	public void setAcceleration(Vector2 acceleration) {
-		this.acceleration = acceleration;
+	public void setVelocity(Vector2 velocity) {
+		this.velocity = velocity;
+	}
+
+	/**
+	 * setter for the weight parameter of the object
+	 * 
+	 * @param height
+	 *            the new weight for the object
+	 */
+	public void setWeight(float weight) {
+		this.weight = weight;
 	}
 }
